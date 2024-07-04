ec289b450e0657c2c531617b3e52cf40e6c78483
Josh <josh@joshthomas.dev>
2024-07-03 20:55:09 -0500

    fix 1password location

--- config/files/usr/libexec/1password-watchdog.sh
+++ config/files/usr/libexec/1password-watchdog.sh
@@ -4,7 +4,7 @@ set -eou pipefail
 
 while true; do
   if ! pgrep -x "1password" >/dev/null; then
-    /opt/1Password/1password --silent --ozone-platform-hint=auto --enable-features=VaapiVideoDecodeLinuxGL,WebRTCPipeWireCapturer %U &
+    /usr/bin/1password --silent --ozone-platform-hint=auto --enable-features=VaapiVideoDecodeLinuxGL,WebRTCPipeWireCapturer %U &
   fi
   sleep 60
 done
