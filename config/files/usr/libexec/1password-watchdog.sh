4928668118bdaedcfa1bb7075b1abda48f834cdf
Josh Thomas <josh@joshthomas.dev>
2024-07-03 21:19:37 -0500

    Update 1password-watchdog.sh

--- config/files/usr/libexec/1password-watchdog.sh
+++ config/files/usr/libexec/1password-watchdog.sh
@@ -2,9 +2,16 @@
 
 set -eou pipefail
 
+ONEPASSWORD_BIN=$(command -v 1password)
+
+if [ -z "$ONEPASSWORD_BIN" ] || [ ! -x "$ONEPASSWORD_BIN" ]; then
+    echo "1Password binary not found in PATH or not executable"
+    exit 1
+fi
+
 while true; do
   if ! pgrep -x "1password" >/dev/null; then
-    /usr/bin/1password --silent --ozone-platform-hint=auto --enable-features=VaapiVideoDecodeLinuxGL,WebRTCPipeWireCapturer %U &
+    "$ONEPASSWORD_BIN" --silent --ozone-platform-hint=auto --enable-features=VaapiVideoDecodeLinuxGL,WebRTCPipeWireCapturer &
   fi
   sleep 60
 done
