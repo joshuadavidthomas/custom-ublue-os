e562ef51652b5bb39f9df17ef58d2f87fa852f30
Josh Thomas <josh@joshthomas.dev>
2024-07-06 18:15:37 -0400

    fix terminal changes (#24)

--- config/scripts/config-terminal.sh
+++ config/scripts/config-terminal.sh
@@ -2,13 +2,13 @@
 
 set -ouex pipefail
 
-sed -i '/<entry name="launchers" type="StringList">/,/<\/entry>/ s/<default>[^<]*<\/default>/<default>preferred:\/\/browser,preferred:\/\/terminal,applications:org.kde.discover.desktop,preferred:\/\/filemanager<\/default>/' /usr/share/plasma/plasmoids/org.kde.plasma.taskmanager/contents/config/main.xml
-sed -i '/<entry name="favorites" type="StringList">/,/<\/entry>/ s/<default>[^<]*<\/default>/<default>preferred:\/\/browser,preferred:\/\/terminal,systemsettings.desktop,org.kde.dolphin.desktop,org.kde.kate.desktop,org.kde.discover.desktop<\/default>/' /usr/share/plasma/plasmoids/org.kde.plasma.kickoff/contents/config/main.xml
+sed -i '/<entry name="launchers" type="StringList">/,/<\/entry>/ s/<default>[^<]*<\/default>/<default>preferred:\/\/browser,applications:org.wezfurlong.wezterm.desktop,applications:org.kde.discover.desktop,preferred:\/\/filemanager<\/default>/' /usr/share/plasma/plasmoids/org.kde.plasma.taskmanager/contents/config/main.xml
+sed -i '/<entry name="favorites" type="StringList">/,/<\/entry>/ s/<default>[^<]*<\/default>/<default>preferred:\/\/browser,applications:org.wezfurlong.wezterm.desktop,systemsettings.desktop,org.kde.dolphin.desktop,org.kde.kate.desktop,org.kde.discover.desktop<\/default>/' /usr/share/plasma/plasmoids/org.kde.plasma.kickoff/contents/config/main.xml
 
-sed -i 's@Keywords=shell;prompt;command;commandline;cmd;@Keywords=shell;prompt;command;commandline;cmd;konsole;console;@g' /usr/share/applications/org.wezfurlong.wezterm.desktop
-sed -i 's@\[Desktop Action new-window\]@\[Desktop Action new-window\]\nX-KDE-Shortcuts=Ctrl+Alt+T@g' /usr/share/applications/org.wezfurlong.wezterm.desktop
+sed -i 's@Keywords=@Keywords=konsole;console;@g' /usr/share/applications/org.wezfurlong.wezterm.desktop
+sed -i '${/\[Desktop Action new-window\]/!s/$/\n\n[Desktop Action new-window]\nX-KDE-Shortcuts=Ctrl+Alt+T/}' /usr/share/applications/org.wezfurlong.wezterm.desktop
 cp /usr/share/applications/org.wezfurlong.wezterm.desktop /usr/share/kglobalaccel/org.wezfurlong.wezterm.desktop
 
-sed -i 's@\[Desktop Action new-window\]\nX-KDE-Shortcuts=Ctrl+Alt+T@\[Desktop Action new-window\]@g' /usr/share/applications/org.gnome.Ptyxis.desktop
+sed -i '/X-KDE-Shortcuts=Ctrl+Alt+T/d' /usr/share/applications/org.gnome.Ptyxis.desktop
 sed -i 's@\[Desktop Entry\]@\[Desktop Entry\]\nNoDisplay=true@g' /usr/share/applications/org.gnome.Ptyxis.desktop
 rm -f /usr/share/kglobalaccel/org.gnome.Ptyxis.desktop
