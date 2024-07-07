a22c298a392c55c5ab2d974b79b94579e6229076
Josh <josh@joshthomas.dev>
2024-07-06 21:27:03 -0400

    try to add the shortcut to the desktop entry

--- config/scripts/config-terminal.sh
+++ config/scripts/config-terminal.sh
@@ -6,14 +6,8 @@ sed -i '/<entry name="launchers" type="StringList">/,/<\/entry>/ s/<default>[^<]
 sed -i '/<entry name="favorites" type="StringList">/,/<\/entry>/ s/<default>[^<]*<\/default>/<default>preferred:\/\/browser,applications:org.wezfurlong.wezterm.desktop,systemsettings.desktop,org.kde.dolphin.desktop,org.kde.kate.desktop,org.kde.discover.desktop<\/default>/' /usr/share/plasma/plasmoids/org.kde.plasma.kickoff/contents/config/main.xml
 
 sed -i 's@Keywords=@Keywords=konsole;console;@g' /usr/share/applications/org.wezfurlong.wezterm.desktop
-cat <<EOF >>/usr/share/applications/org.wezfurlong.wezterm.desktop
-
-[Desktop Action new-window]
-X-KDE-Shortcuts=Ctrl+Alt+T
-Name=New Window
-Exec=wezterm start
-EOF
-cp /usr/share/applications/org.wezfurlong.wezterm.desktop /usr/share/kglobalaccel/org.wezfurlong.wezterm.desktop
+sed -i 's@\[Desktop Entry\]@\[Desktop Entry\]\nX-KDE-Shortcuts=Ctrl+Alt+T@g' /usr/share/applications/org.wezfurlong.wezterm.desktop
+ln -s /usr/share/applications/org.wezfurlong.wezterm.desktop /usr/share/kglobalaccel/org.wezfurlong.wezterm.desktop
 
 sed -i '/X-KDE-Shortcuts=Ctrl+Alt+T/d' /usr/share/applications/org.gnome.Ptyxis.desktop
 sed -i 's@\[Desktop Entry\]@\[Desktop Entry\]\nNoDisplay=true@g' /usr/share/applications/org.gnome.Ptyxis.desktop
